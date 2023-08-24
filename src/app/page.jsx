c0f1c1637ae942f851584d50dd7630fafa955e78
AndreeaKiara <69343737+AndreeaKiara@users.noreply.github.com>
2023-08-24 19:24:05 +0300

    next.js install + set-up

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+
+export default function Home() {
+  return (
+    <>
+      <header className="header">
+        <Header></Header>
+      </header>
+
+      <main className="content"></main>
+
+      <footer className="footer">
+        <Footer></Footer>
+      </footer>
+    </>
+  );
+}
